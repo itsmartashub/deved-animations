94e093ee7110b1723be9fe6ac4971850f4066fe8
master <coderawii@gmail.com>
2023-04-27 18:00:56 +0200

    [parallax_scroll_iphone]: #6 parallax effects ✅

--- paralax_scroll_iphone/app.js
+++ paralax_scroll_iphone/app.js
@@ -129,3 +129,16 @@ const tlVideo = gsap.timeline({
 })
 tlVideo.fromTo('.product-video', { currentTime: 0 }, { currentTime: 3, duration: 1 }) // 3 je jer video traje 3 sekunde
 tlVideo.fromTo('.product-info-container h3', { opacity: 0 }, { opacity: 1, stagger: 0.25, duration: 0.5 }, '<')
+
+// #6 PAGE
+const tlParallax = gsap.timeline({
+	scrollTrigger: {
+		trigger: '.sixth-page',
+		start: '-25%',
+		end: '50%',
+		scrub: true,
+	},
+})
+tlParallax.fromTo('.photo-description', { y: 0 }, { y: -80 })
+tlParallax.fromTo('.portrait-container', { y: 0 }, { y: -80 }, '<')
+tlParallax.fromTo('.phone-video', { y: 0 }, { y: -400 }, '<')
